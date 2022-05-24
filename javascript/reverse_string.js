b735c8098ada848548f08fa27899b452d226fa18
Tucker Steil <tuckersteil@gmail.com>
2022-05-24 13:32:16 -0400

    commit tree c45ecca700abb78c7468b479404f51938e8d9159

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,12 +1,21 @@
+// str = "cat"
+// str = "tucker"
 function reverseString(str) {
-  // type your code here
+  let array1 = []
+  let string = ""
+  for (i=0; i < str.length; i++){
+      array1.unshift(str[i])
+  }
+  string = array1.join('')
+
+  return string
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
-
+  console.log(array1)
   console.log("");
 
   console.log("Expecting: 'ybabtac'");
